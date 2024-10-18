9230b2dcbf894fb004d9bd261cbc3ebe39c61fe4
Ben Herila <ben+gh@herila.net>
2024-10-17 23:53:56 -0700

    Add upload command, footer adjustments

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,13 +9,13 @@ export default function Footer() {
         <div className="block">
           <div className="block sm:inline text-nowrap">
             <i className={cn(hColor)}>Call:</i>
-            <a href="tel:908-963-0983" className="text-white px-1 font-bold">
+            <a href="tel:908-963-0983" className="text-white px-2 font-bold">
               908-963-0983
             </a>
           </div>
-          <div className="block sm:inline text-nowrap lg:px-4">
+          <div className="block sm:inline text-nowrap lg:pl-4">
             <i className={cn(hColor)}>Email:</i>
-            <a href="mailto:gil@roessner.net" className="text-white px-1 font-bold">
+            <a href="mailto:gil@roessner.net" className="text-white px-2 font-bold">
               gil@roessner.net
             </a>
           </div>
